b7d125c29dfecbe023a63b5de9a09f8146b72ef4
chenxiang <chenxiang@huoqiu.cn>
2017-07-14 09:59:11 +0800

    增加三个爬虫页

---
+++ src/components/spider/news5.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+
+
+			<table width="100%">
+
+				<tbody><tr><td>
+
+<div style=" float:right; width:305px; height:255px;"></div>
+
+<p>    [ wagons光速超跑长沙行情]  近日，编辑从长沙瑞宝汽车销售服务有限公司获悉，宝马Z4新增的两款车型sDrive30i领先型和sDrive35i豪华型店内已经开始接受预订，订金10万元，预订周期为2-3个月，感兴趣的朋友请联系经销商到店赏车试驾。详情如下表：</p><p align="center"></p><table id="_DevelopTable" style="FONT-SIZE: 13px; BORDER-COLLAPSE: collapse; TEXT-ALIGN: center" bordercolor="#000000" cellspacing="0" cellpadding="0" width="500" align="center" border="1"><tbody><tr height="35"><td style="FONT-WEIGHT: bold" width="500" bgcolor="#d6ebff" colspan="5" isshow="false">2010款宝马Z4车型最新价格变化报价</td></tr><tr height="35"><td style="FONT-WEIGHT: bold" width="99" isshow="false">车型</td><td style="FONT-WEIGHT: bold" width="99" isshow="false">指导价<br>(万元)</td><td style="FONT-WEIGHT: bold" width="99" isshow="false">现价<br>(万元)</td><td style="FONT-WEIGHT: bold" width="99" isshow="false">优惠金额<br>(万元)</td><td style="FONT-WEIGHT: bold" width="99" isshow="false">现车情况</td></tr><tr height="35"><td width="99" isshow="false">2010款 <br>sDrive30i领先型</td><td width="99" isshow="false">67.60</td><td width="99" isshow="false">67.60</td><td width="99" isshow="false">-</td><td width="99" isshow="false">需预订</td></tr><tr><td width="99" height="35" isshow="false">2010款 <br>sDrive35i豪华型</td><td width="99" height="35" isshow="false">78.80</td><td width="99" height="35" isshow="false">78.80</td><td width="99" height="35">-</td><td width="99" height="35" isshow="false">需预订</td></tr><tr height="35"><td width="500" colspan="5" isshow="false">订金10万元，预订周期为2-3个月。</td></tr><tr height="35"><td id="DevelopTableParentYear" style="FONT-WEIGHT: bold" width="500" colspan="5" isshow="false">2010年06月02日行情 车辆价格随时变动，敬请关注当地市场</td></tr></tbody></table><p></p><p align="center"><img alt="宝马 宝马(进口) 宝马Z4 2010款 sDrive30i领先型" src="/Image/2010072504215112301.jpg"></p><p align="center">『宝马(进口) 宝马Z4 2010款 sDrive30i领先型』</p><p>    宝马Z4享受厂家提供的两年不限公里整车质保，4S店建议的日常保养周期为8000-10000公里，常规保养费用在1500元以内，每行驶3万公里做一次大保养，大保养的费用视具体车况而定，需以店内专业人员定价为准。</p><p align="center"><img alt="宝马 宝马(进口) 宝马Z4 09款 sDrive35i锋尚型" src="/Image/2010072504215112302.jpg">   <img alt="宝马 宝马(进口) 宝马Z4 09款 sDrive35i锋尚型" src="/Image/2010072504215112303.jpg"></p><p align="center"><img alt="宝马 宝马(进口) 宝马Z4 09款 sDrive35i锋尚型" src="/Image/2010072504215112304.jpg">   <img alt="宝马 宝马(进口) 宝马Z4 09款 sDrive35i锋尚型" src="/Image/2010072504215112305.jpg"></p><p>    2010款宝马Z4第一年的全险费用根据车型不同大约在1.9-2.2万元之间，店内为顾客提供平安保险的服务；想贷款购车的朋友，店内提供深发展银行的服务，一般能申请到首付最少30%，最长3年还清的服务。以67.60万的2010款sDrive30i领先型为例粗略计算，需要首付约20.3万元，月供在1.53万元左右，比全款购车多花费约7.7万元。
+
+</p><div align="center"></div> 
+
+</td></tr>
+
+			</tbody></table>
+
+			
+  </div>
+</template>
+<script>
+  export default{
+    name:'1'
+  }
+</script>
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 const VueAwesomeSwiper = require('vue-awesome-swiper');
-import Hello from '@/components/Hello';
 // import test from '../components/test/test.vue' //直接加载方式，组件写在公共js里，如果这种方式过多会是的文件提过大，首次加载变慢
 // import page from '../components/test/page.vue'
-const test = resolve => require(['../components/test/test.vue'], resolve);
-const page = resolve => require(['../components/test/page.vue'], resolve); //懒加载方式，组件不会被打包进公共js ，而是另外请求
-const ven = resolve => require(['../components/test/ven.vue'], resolve);
 const pcweb = resolve => require(['../components/pcweb/pcweb.vue'], resolve);
 const pcweb_first = resolve => require(['../components/pcweb/pcweb_first.vue'], resolve);
 const pcweb_second = resolve => require(['../components/pcweb/pcweb_second.vue'], resolve);
@@ -16,6 +12,9 @@ const pcweb_second = resolve => require(['../components/pcweb/pcweb_second.vue']
 const news1 = resolve => require(['../components/spider/news1.vue'], resolve);
 const news2 = resolve => require(['../components/spider/news2.vue'], resolve);
 const news3 = resolve => require(['../components/spider/news3.vue'], resolve);
+const news4 = resolve => require(['../components/spider/news4.vue'], resolve);
+const news5 = resolve => require(['../components/spider/news5.vue'], resolve);
+const news6 = resolve => require(['../components/spider/news6.vue'], resolve);
 
 
 
@@ -25,19 +24,6 @@ Vue.use(Router);
 export default new Router({
     mode: 'history',
     routes: [
-        //hello页
-        { path: '/hello', component: Hello },
-        //test嵌套路由测试
-        {
-            path: '/test',
-            component: test,
-            children: [
-                { path: 'ven', component: ven },
-
-                { path: 'foo', component: page },
-                // {	path: '',component: page	},
-            ]
-        },
         //重写首页
         {
             path: '/',
@@ -61,6 +47,9 @@ export default new Router({
         { path: '/supercarnews/1', component: news1 },
         { path: '/supercarnews/2', component: news2 },
         { path: '/supercarnews/3', component: news3 },
+        { path: '/supercarnews/4', component: news4 },
+        { path: '/supercarnews/5', component: news5 },
+        { path: '/supercarnews/6', component: news6 },
 
 
 
