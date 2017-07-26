2210cb3d7d02829748bb679b7989a7af973a0b0e
chenxiang <chenxiang@huoqiu.cn>
2017-07-26 12:51:16 +0800

    mobile

--- src/components/404.vue
+++ src/components/404.vue
@@ -1,20 +1,31 @@
 <template>
     <div>
-        <div v-if='ish5'>h5 page not found</div>
+        <div class="h5" v-if='ish5'>
+            <m-head></m-head>
+             <a @click="goback" class="goback" id="goback"></a>
+            <img class="h5img" src="../assets/car_pc_404.png">
+            <p>页面找不到了</p>
+            <p style="padding-top:0.2rem">这里的跑车早被别人捷足先登啦</p>
+            <div @click="tomobile" class="but">返回首页</div>
+        </div>
         <div class="pc" v-else>
             <img class="pcimg" src="../assets/car_pc_404.png" /> 
             <p>页面找不到了</p>
             <p style="padding-top:20px">这里的跑车早被别人捷足先登啦</p>
         </div> 
-        <router-link class="back" to="/first">返回首页</router-link>
     </div>  
 </template>
 <script>
+require('./mobile/rem.js')(window,document)
+import head from './mobile/header.vue'
 export default {
     data(){
         return {
             ish5:'true'
         }
+    },
+    components:{
+        'm-head':head
     },
     created(){
         if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
@@ -24,10 +35,54 @@ export default {
             }
     },
     methods:{
+        tomobile(){
+            this.$router.push('/mobile')
+        },
+        goback(){
+            this.$router.go(-1)
+        }
     }
 }
 </script>
 <style scoped>
+.goback {
+    z-index: 10000;
+    position: absolute;
+    left: 0.1rem;
+    top: 0.33rem;
+    width: 1rem;
+    height: 0.8rem;
+    background: url('../assets/m-goback.png') no-repeat center center;
+    background-size: 0.3rem 0.3rem;
+}
+.h5 .but{
+    position: absolute;
+    bottom: 1.5rem;
+    left: 3rem;
+    margin: auto;
+    border: 1px solid #009cff;
+    display: block;
+    width: 1.53rem;
+    height: 0.54rem;
+    font-size: 0.24rem;
+    text-align: center;
+    color: #009cff;
+    border-radius: 0.5rem;
+    line-height: 0.5rem;
+}
+.h5 p{
+    position: relative;
+    top:-1.3rem;
+    color: #cacaca;
+    text-align: center;
+    font-size: 0.26rem;
+}
+.h5img{
+    width:5.4rem; 
+    display: block;
+    margin: auto;
+    margin-top: 57%;
+}
 .back{
     margin: auto;
     text-align: center;
--- src/main.js
+++ src/main.js
@@ -14,8 +14,8 @@ Vue.use(VueLazyload)
 // use options
 Vue.use(VueLazyload, {
     preLoad: 1.3,
-    error: '/assets/3.png',
-    loading: 'http://cdn.uehtml.com/201402/1392662591495_1140x0.gif',
+    error: './assets/loading12.png',
+    loading: './assets/loading12.png',
     attempt: 1,
     listenEvents: ['scroll', 'mousewheel']
 })
