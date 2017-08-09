de4515568f6d77a8b2b0669a70fca16abc9fd05c
chenxiang <chenxiang@huoqiu.cn>
2017-08-09 14:27:06 +0800

    ss

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,7 +2,7 @@ var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
-import 'babel-polyfill'
+require('babel-polyfill')
 
 function resolve(dir) {
     return path.join(__dirname, '..', dir)
