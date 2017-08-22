88e15db39e9af178c8a850a774b271296c2ea4bc
chenxiang <chenxiang@huoqiu.cn>
2017-08-22 11:17:49 +0800

    aa

--- src/components/app/memjoin.vue
+++ src/components/app/memjoin.vue
@@ -15,6 +15,21 @@
         <h3><span></span><p>会员长期有效，退费时无效</p></h3>
         <h3><span></span><p>个别车型需要补交押金可以通过pos预授权、支付宝或微信支付、银行卡支付，冻结火球资金等方式扣取</p></h3>
     </div>
+    <div class="m">
+    <p>Method 2</p>
+        <h1></h1>
+        <h2>购买合作方私募或公募基金</h2>
+        <table class="tb1">
+            <tr><td style="vertical-align: middle;width:2.35rem;border-right: 1px solid #3d454d;">银卡会员</td><td class="ttt" style="width:4.75rem">仅支持缴纳会费形式申请</td></tr>
+            <tr style="border-bottom: 1px solid #3d454d;border-top: 1px solid #3d454d;"><td style="vertical-align: middle;border-right: 1px solid #3d454d;">金卡会员</td><td class="ttt" >购买或已持有合作方私募基金<span>100万</span>（一年起）及以上或<br>购买或已持有合作方公募基金<span>100万</span>（一年起）及以上</td></tr>
+            <tr><td  style="vertical-align: middle;border-right: 1px solid #3d454d;">白金会员</td><td class="ttt">购买或已持有合作方私募基金<span>200万</span>（一年起）及以上或<br>购买或已持有合作方公募基金<span>200万</span>（一年起）及以上</td></tr>
+        </table>
+        <a class="simu" href="https://toweringcapital.cn/?pcode=wagons">点此了解或购买合作方私募基金</a>
+        <h3><span></span><p>用户在用车时需确保持有基金份额达到指定份额</p></h3>
+        <h3><span></span><p>成为会员后租车无需缴纳押金（个别车型除外）</p></h3>
+        <h3><span></span><p>会员在基金持有期间有效</p></h3>
+        <h3><span></span><p>个别车型需要补交押金可以通过pos预授权、支付宝或微信支付、银行卡支付，冻结火球资金等方式扣取</p></h3>
+    </div>
 </div>
 </template>
 
@@ -25,10 +40,28 @@ export default {
 </script>
 
 <style scoped>
+.simu{
+    -webkit-tap-highlight-color:transparent;
+     font-size: 0.24rem;
+    display: block;
+    margin-left: 0.4rem;
+    color: cornflowerblue;
+    margin-top: 0.3rem;
+    text-decoration: none;
+    margin-bottom: 0.1rem;
+}
+.ttt{
+        padding-left: 0.45rem;
+    padding-right: 0.5rem;
+    text-align: left;
+    line-height: 0.5rem;
+    padding-top: 0.32rem;
+    padding-bottom: 0.32rem;
+}
 .m h3 p{
         display: inline-block;
         width: 6.2rem;
-        line-height: 0,5rem;
+        line-height: 0.5rem;
 }
 .m h3 span{
     background: #999999;
@@ -36,13 +69,11 @@ export default {
     height: 4px;
     display: inline-block;
     border-radius: 10px;
-    position: relative;
-    top: -2px;
     margin-left: 10px;
     margin-right: 0.3rem;
-        vertical-align: top;
-        position: relative;
-        top: 5px;
+    vertical-align: top;
+    position: relative;
+    top: 10px;
 }
 .m h3{
     color: #999999;
@@ -50,8 +81,8 @@ export default {
     width: 7.1rem;
     display: block;
     margin: auto;
-    margin-top: 0.3rem;
-    margin-bottom: 0.3rem;
+    /* margin-top: 0.3rem; */
+    /* margin-bottom: 0.3rem; */
 }
 .tb1 span{
     color: #fcd82f;
@@ -67,10 +98,10 @@ export default {
     margin: auto;
     text-align: center;
     font-size: 0.24rem;
-    color: #ffffff;
+    color: #acacac;
     background: #273039;
     margin-top: 0.32rem;
-    
+    margin-bottom: 0.28rem;
 }
 .m>h2{
     color: #fcd82f;
@@ -107,7 +138,7 @@ font-size: 0.28rem;
 .g1{
 background: #273039;
     font-size: 0.24rem;
-    color: #ffffff;
+    color: #acacac;
     height: 1.5rem;
     width: 7.1rem;
     margin: auto;
