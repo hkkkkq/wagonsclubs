eacb2170f8932d1b3844524b4e2a7e1c49011daa
chenxiang <chenxiang@huoqiu.cn>
2017-09-18 10:21:49 +0800

    去掉苹方字体

--- src/components/mobile/index.vue
+++ src/components/mobile/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div style="font-family: PingFangSC-Medium, sans-serif;" class="body">
+  <div class="body">
+      <!-- style="font-family: PingFangSC-Medium, sans-serif;" -->
       <m-header></m-header>
       <pull :maxDistance=60 :topDistance=50 :top-method="loadTop" @top-status-change="handleTopChange" ref="loadmore">
         <m-list  v-if="foot_left"></m-list>   
