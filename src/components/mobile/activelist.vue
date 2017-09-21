ddd48f14dd177c0e29250372ccb3bf54ca97ba02
chenxiang <chenxiang@huoqiu.cn>
2017-09-21 17:00:20 +0800

    goo

--- src/components/mobile/activelist.vue
+++ src/components/mobile/activelist.vue
@@ -133,7 +133,7 @@ export default {
                 setTimeout(function() {
                     goo(n)
                 }, 2000);
-            }else{
+            } else {
                 goo(n)
             }
 
