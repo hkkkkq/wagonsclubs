78dbec5e175173f357f2cc9e9aef795262d0ca66
chenxiang <chenxiang@huoqiu.cn>
2017-11-13 10:43:54 +0800

    card

--- src/components/wx/test.vue
+++ src/components/wx/test.vue
@@ -2,37 +2,37 @@
   <div>
     <div v-if="env == 'development'" >
       <br>
-      <a :href="'http://localhost:8080/wx/findcar?WAG='+WAG">找车</a>
+      <a :href="'http://localhost:8080/wx/findcar?WAG='+W">找车</a>
       <br>
-      <a :href="'http://localhost:8080/wx/hd?WAG='+WAG">活动</a>
+      <a :href="'http://localhost:8080/wx/hd?WAG='+W">活动</a>
       <br>
       会员中心
       <br>
       <a :href='"http://localhost:8080/app/ab"'>关于我们</a>
       <br>
-      <a :href="'http://localhost:8080/wx/wxmember?WAG='+WAG">会员权益</a>
+      <a :href="'http://localhost:8080/wx/wxmember?WAG='+W">会员权益</a>
       <br>
-      <a :href="'http://localhost:8080/wx/orders?WAG='+WAG">订单查询</a>
+      <a :href="'http://localhost:8080/wx/orders?WAG='+W">订单查询</a>
       <br>
-      <a :href="'http://localhost:8080/wx/card?WAG='+WAG">充值卡</a>
+      <a :href="'http://localhost:8080/wx/card?WAG='+W">充值卡</a>
       <br>
       <a :href="'http://localhost:8080/app/warning'">用车须知</a>
     </div>
     <div v-else>
       <br>
-      <a :href="'http://www.test.wagonsclub.com/wx/findcar?WAG='+WAG">找车</a>
+      <a :href="'http://www.test.wagonsclub.com/wx/findcar?WAG='+W">找车</a>
       <br>
-      <a :href="'http://www.test.wagonsclub.com/wx/hd?WAG='+WAG">活动</a>
+      <a :href="'http://www.test.wagonsclub.com/wx/hd?WAG='+W">活动</a>
       <br>
       会员中心
       <br>
       <a :href='"http://www.test.wagonsclub.com/app/ab"'>关于我们</a>
       <br>
-      <a :href="'http://www.test.wagonsclub.com/wx/wxmember?WAG='+WAG">会员权益</a>
+      <a :href="'http://www.test.wagonsclub.com/wx/wxmember?WAG='+W">会员权益</a>
       <br>
-      <a :href="'http://www.test.wagonsclub.com/wx/orders?WAG='+WAG">订单查询</a>
+      <a :href="'http://www.test.wagonsclub.com/wx/orders?WAG='+W">订单查询</a>
       <br>
-      <a :href="'http://www.test.wagonsclub.com/wx/card?WAG='+WAG">充值卡</a>
+      <a :href="'http://www.test.wagonsclub.com/wx/card?WAG='+W">充值卡</a>
       <br>
       <a :href="'http://www.test.wagonsclub.com/app/warning'">用车须知</a>
     </div>
@@ -45,7 +45,7 @@ export default {
   data() {
     return {
       env:'',
-      WAG:'oEUUVv1Fko_nef3hzP9KY_P7nDHM'
+      W:'oEUUVv1Fko_nef3hzP9KY_P7nDHM'
     };
   },
   created() {
