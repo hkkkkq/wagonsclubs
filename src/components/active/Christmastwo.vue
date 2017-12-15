c3aa7e7fae0a6e392c97fd2c660314f6cca7c8bb
chenxiang <chenxiang@huoqiu.cn>
2017-12-15 18:35:28 +0800

    WAG-000:链接写死

--- src/components/active/Christmastwo.vue
+++ src/components/active/Christmastwo.vue
@@ -127,6 +127,13 @@ export default {
         ]
       });
       var locationHref = window.location.href;
+      wx.checkJsApi({
+        jsApiList: ['onMenuShareTimeline'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+        success: function(res) {
+          // 以键值对的形式返回，可用的api值true，不可用为false
+          // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+          }
+      });
       wx.ready(function() {
         wx.onMenuShareTimeline({
           title: 'WAGONS光速超跑圣诞节活动',
